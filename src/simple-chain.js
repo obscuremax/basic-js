94c78771c60c5b77d136ffd5a63d17ebd3074732
obscuremax <maxxam@yandex.ru>
2020-02-21 04:59:54 +0300

    resolve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,35 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+	ch: [],
+	getLength() {
+	  return this.ch.length
+	},
+	addLink(value) {
+	  this.ch.push(`( ${value} )`)
+	  return this
+	},
+	removeLink(position) {
+	  if(position !== Number(position) || 0>=position || position>this.ch.length-1) {
+	    this.ch = []
+	    throw "error"
+    } else {
+	    this.ch.splice(position-1,1)
+      return this
+    }
+	},
+	reverseChain() {
+	  this.ch.reverse()
+    return this
+	},
+	finishChain() {
+	  let newCh = this.ch.map((el, index) => {
+	    if (index < this.ch.length-1) {
+        return `${el}~~`
+      } else {
+         return `${el}`
+      }
+    }).join("")
+    this.ch = []
+    return newCh
+	}
 };
-
 module.exports = chainMaker;
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,54 @@
-module.exports = function transform(/* arr */) {
+module.exports = function transform(arr) {
     throw 'Not implemented';
-    // remove line with error and write your code here
+
+    // if (arr === Number(arr) || arr === null || arr === false || arr === undefined || arr.length === undefined) {
+    //     throw "err"
+    // } else if (arr[0] === undefined ){
+    //     return []
+    // } else {
+    //     let newArr = []
+    //
+    //
+    //     for (let i=0; i<arr.length; i++) {
+    //         if (arr.length === 0){
+    //             newArr=[+arr[0]]
+    //         }
+    //         if (arr[i] === "--discard-next") {
+    //             if (arr[i+1] !== undefined) {
+    //                 arr.splice(i+1,1, undefined)
+    //                 // newArr.push(arr[i+1])
+    //             } else {
+    //                 break
+    //             }
+    //
+    //         } else if (arr[i] === "--discard-prev" ) {
+    //             if (newArr.pop() !== undefined) {
+    //                 newArr.pop()
+    //             } else {
+    //                 break
+    //             }
+    //         } else if (arr[i] === "--double-next" && i !== 0) {
+    //             if (arr[i+1] !== undefined) {
+    //                 newArr.push( arr[i+1])
+    //                 newArr.push( arr[i+1])
+    //             } else {
+    //                 break
+    //             }
+    //
+    //         } else if (arr[i] === "--double-prev" ) {
+    //             if (newArr.pop() !== undefined) {
+    //                 let doble = newArr.pop()
+    //                 newArr = [...newArr, doble, doble]
+    //             } else {
+    //                 break
+    //             }
+    //
+    //         } else {
+    //             newArr.push(arr[i])
+    //         }
+    //
+    //
+    //     }
+    //     return newArr
+    // }
 };
